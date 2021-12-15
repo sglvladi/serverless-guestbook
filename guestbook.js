7db9615e74512a8931cab3c2e87561af95f6dff4
sglvladi <sglvladi@gmail.com>
2021-12-15 01:57:12 +0000

    Added api url

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://dab08da7.eu-gb.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
